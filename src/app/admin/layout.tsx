c6c645a8f99062035f227621b391b369145d55dc
Shruti Ezhava <shruti10girish@gmail.com>
2026-02-17 07:47:06 +0530

    Admin routes modified

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,6 @@
-
 import AdminSidebar from '@/components/AdminSidebar';
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function AdminLayout({
     children,
